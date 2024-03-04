004a3cd1f0aff7557e135877ad156d71b8b12ec7
Vikash Kumar Pandey <112485773+Vikash2984@users.noreply.github.com>
2024-03-04 19:43:50 +0530

    Update and rename OOPs.py to Hybrid class.py
    Segregated Hybrid class and Abstract class's source code

---
+++ Hybrid class.py
@@ -0,0 +1,28 @@
+# #Hybrid class
+# class Base:
+#     def __init__(self):
+#         print ("Base: Constructor method is executing...")
+#     def displayB(self):
+#         print ("Base: Display method is executing...")
+
+# class Mid1(Base):
+#     def __init__(self):
+#         print ("Mid1: Constructor method is executing...")
+#     def displayB(self):
+#         print ("Mid1: Display method is executing...")
+
+# class Mid2(Base):
+#     # def __init__(self):
+#     #     print ("Mid2: Constructor method is executing...")
+#     def displayB(self):
+#         print ("Mid2: Display method is executing...")
+
+# class Derived(Mid1,Mid2):
+#     def displayD1(self):
+#         print ("Derived: Display method is executing...")        
+
+
+# ob1 = Derived()
+# print()
+# ob2 = Mid2()
+# super(Derived,ob1).__init__() 
